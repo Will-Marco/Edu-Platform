0db1632cf6265bb10b853e110ac113b997cdf1e8
Will-Marco <mr.marco0101@gmail.com>
2023-11-18 22:55:47 +0500

    Revert "changed some navbar style and add function"
    This reverts commit 695e5abff28176aaa6552e3b6e4e1cf15c8cf6dd.

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = ({ window }: NavbarProps) => {
   };
 
   const drawer = (
-    <Box sx={{ textAlign: "center" }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Box
         sx={{
           px: "20px",
@@ -40,20 +40,17 @@ const Navbar = ({ window }: NavbarProps) => {
       >
         <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
           <SchoolIcon />
-          <Typography variant="h4" sx={{ my: 2 }} paddingLeft={'7px'}>
+          <Typography variant="h6" sx={{ my: 2 }}>
             Edu-P
           </Typography>
         </Box>
-        <CloseIcon onClick={handleDrawerToggle} sx={{ cursor: "pointer" }} />
+        <CloseIcon />
       </Box>
       <Divider />
       <List>
         {navItems.map((item) => (
           <ListItem key={item.route} disablePadding>
-            <ListItemButton
-              onClick={() => router.push(item.route)}
-              sx={{ textAlign: "center" }}
-            >
+            <ListItemButton sx={{ textAlign: "center" }}>
               <ListItemText primary={item.label} />
             </ListItemButton>
           </ListItem>
@@ -66,9 +63,9 @@ const Navbar = ({ window }: NavbarProps) => {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box height={"9vh"} sx={{ display: "flex" }}>
+    <Box height={"10vh"} sx={{ display: "flex" }}>
       <AppBar
-        sx={{ height: "9vh", backgroundColor: "#141414" }}
+        sx={{ height: "10vh", backgroundColor: "#141414" }}
         component="nav"
       >
         <Toolbar>
@@ -82,18 +79,15 @@ const Navbar = ({ window }: NavbarProps) => {
             <MenuIcon />
           </IconButton>
           <Box
-            onClick={() => router.push("/")}
             sx={{
-              my: 2,
-              display: "flex",
-              alignItems: "center",
-              gap: "5px",
               flexGrow: 1,
-              cursor: "pointer",
+              display: { xs: "none", sm: "flex" },
+              alignItems: "center",
+              gap: "10px",
             }}
           >
             <SchoolIcon />
-            <Typography variant="h4" paddingLeft={'7px'}>Edu-P</Typography>
+            <Typography variant="h6">Edu-P</Typography>
           </Box>
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
