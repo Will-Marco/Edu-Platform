b7f826e0c5369b8a32fc3f1d754fd3360180646a
Will-Marco <mr.marco0101@gmail.com>
2023-11-18 21:25:32 +0500

    add NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Head from "next/head";
 import createEmotionCache from "../helpers/create-emotion-cache";
 import theme from "../helpers/theme";
+import { useEffect } from "react";
+import nProgress from "nprogress";
+import { Router } from "next/router";
 const clientSideEmotionCache = createEmotionCache();
+import "nprogress/nprogress.css";
+import "@/styles/global.css";
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -13,6 +18,21 @@ export interface MyAppProps extends AppProps {
 
 export default function App(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  useEffect(() => {
+    const handleRouteStart = () => nProgress.start();
+    const handleRouteDone = () => nProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
 
   return (
     <CacheProvider value={emotionCache}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,36 +9,45 @@ import SEO from "@/layout/seo/Seo";
 
 const Index = ({ blogs, latestBlogs, categories }: HomePageProps) => {
   return (
-	<SEO>
-    <Layout>
-      <Hero blogs={blogs.slice(0, 3)} />
-      <Box sx={{ display: "flex", gap: "20px", flexDirection: { xs: "column", md: "row" }, padding: "20px" }}>
-        <Sidebar latestBlogs={latestBlogs} categories={categories} />
-		<Content blogs={blogs} />
-      </Box>
-    </Layout>
-	</SEO>
+    <SEO>
+      <Layout>
+        <Hero blogs={blogs.slice(0, 3)} />
+        <Box
+          sx={{
+            display: "flex",
+            gap: "20px",
+            flexDirection: { xs: "column", md: "row" },
+            padding: "20px",
+          }}
+        >
+          <Sidebar latestBlogs={latestBlogs} categories={categories} />
+          <Content blogs={blogs} />
+        </Box>
+      </Layout>
+    </SEO>
   );
 };
 
 export default Index;
 
-export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
-	const blogs = await BlogsService.getAllBlogs();
-	const latestBlogs = await BlogsService.getLatestBlog();
-	const categories = await BlogsService.getCategories();
+export const getServerSideProps: GetServerSideProps<
+  HomePageProps
+> = async () => {
+  const blogs = await BlogsService.getAllBlogs();
+  const latestBlogs = await BlogsService.getLatestBlog();
+  const categories = await BlogsService.getCategories();
 
-	return {
-		props: {
-			blogs,
-			latestBlogs,
-			categories,
-		},
-	};
+  return {
+    props: {
+      blogs,
+      latestBlogs,
+      categories,
+    },
+  };
 };
 
 interface HomePageProps {
-	blogs: BlogsType[];
-	latestBlogs: BlogsType[];
-	categories: CategoryType[];
+  blogs: BlogsType[];
+  latestBlogs: BlogsType[];
+  categories: CategoryType[];
 }
